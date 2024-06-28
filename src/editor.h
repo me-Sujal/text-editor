7085d7b2c2343c49cde8f1b577eecd9df68ac9c5
shrawan077 <121821821+shrawan077@users.noreply.github.com>
2024-06-29 00:53:01 +0545

    Update editor.h
    undo/redo

--- src/editor.h
+++ src/editor.h
@@ -5,12 +5,15 @@
 class Editor : public wxStyledTextCtrl
 {
 public:
-    Editor(wxWindow *parent, wxWindowID id = wxID_ANY,
-           const wxPoint &pos = wxDefaultPosition,
-           const wxSize &size = wxDefaultSize,
-           long style = 0);
+    Editor(wxWindow* parent, wxWindowID id = wxID_ANY,
+        const wxPoint& pos = wxDefaultPosition,
+        const wxSize& size = wxDefaultSize,
+        long style = 0);
+    // methods for undo/redo
+    void Undo();
+    void Redo();
 
 private:
     void SetupEditor();
     void SetupStyles();
-};
+}; 

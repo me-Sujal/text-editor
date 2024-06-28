884f12a3383f05d5a6d4c39c916ca43886004d48
shrawan077 <121821821+shrawan077@users.noreply.github.com>
2024-06-29 00:52:07 +0545

    Update editor.cpp
    adding undo/redo functionality

--- src/editor.cpp
+++ src/editor.cpp
@@ -2,7 +2,7 @@
 #include <wx/stc/stc.h>
 #include "editor.h"
 
-Editor::Editor(wxWindow *parent, wxWindowID id, const wxPoint &pos, const wxSize &size, long style)
+Editor::Editor(wxWindow* parent, wxWindowID id, const wxPoint& pos, const wxSize& size, long style)
     : wxStyledTextCtrl(parent, id, pos, size, style)
 {
     SetupEditor();
@@ -22,10 +22,14 @@ void Editor::SetupEditor()
     // for line numbers
     SetMarginType(0, wxSTC_MARGIN_NUMBER);
     SetMarginWidth(0, 25);
- 
+
+    //to enable undo/redo functionality
+    SetUndoCollection(true);
+    EmptyUndoBuffer();
+
 }
 
-void Editor::SetupStyles(){
+void Editor::SetupStyles() {
 
     // style for entire editor
     StyleSetBackground(wxSTC_STYLE_DEFAULT, wxColour(250, 250, 250)); // style for the editor background
@@ -33,11 +37,25 @@ void Editor::SetupStyles(){
 
     StyleSetForeground(wxSTC_STYLE_LINENUMBER, wxColour(200, 200, 200)); // style for line number 
     StyleSetBackground(wxSTC_STYLE_LINENUMBER, wxColour(62, 78, 110));
-    
+
 
     // To make sure style is refreshed and also without this the background color of text is not as same as editor
     StyleClearAll();
     Refresh();
     Update();
 }
+void Editor::Undo()
+{
+    if (CanUndo())
+    {
+        wxStyledTextCtrl::Undo();
+    }
+}
 
+void Editor::Redo()
+{
+    if (CanRedo())
+    {
+        wxStyledTextCtrl::Redo();
+    }
+}

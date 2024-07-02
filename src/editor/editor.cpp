60a82d659136cc60d50d31f300cf21a26a9c4b19
sujal <sujalpandit1426@gmail.com>
2024-07-02 20:34:51 +0545

    styled editor

--- src/editor/editor.cpp
+++ src/editor/editor.cpp
@@ -19,7 +19,7 @@ void Editor::SetupEditor()
 
     // for line numbers
     SetMarginType(0, wxSTC_MARGIN_NUMBER);
-    SetMarginWidth(0, 25);
+    SetMarginWidth(0, 50);
     SetUndoCollection(true);
     EmptyUndoBuffer();
 }
@@ -27,16 +27,28 @@ void Editor::SetupEditor()
 void Editor::SetupStyles()
 {
     // style for entire editor
-    StyleSetBackground(wxSTC_STYLE_DEFAULT, wxColour(250, 250, 250)); // style for the editor background
-    StyleSetForeground(wxSTC_STYLE_DEFAULT, wxColour(0, 0, 0));       // style for the editor text
-
-    StyleSetForeground(wxSTC_STYLE_LINENUMBER, wxColour(200, 200, 200)); // style for line number
-    StyleSetBackground(wxSTC_STYLE_LINENUMBER, wxColour(62, 78, 110));
+    StyleSetForeground(wxSTC_STYLE_DEFAULT, wxColour(200, 200, 200)); // style for the editor background
+    StyleSetBackground(wxSTC_STYLE_DEFAULT, wxColour(30, 30, 30));       // style for the editor text
+    SetCaretForeground(wxColour(255, 255, 255));
+    StyleSetForeground(wxSTC_STYLE_LINENUMBER, wxColour(75, 75, 75)); // style for line number
+    StyleSetBackground(wxSTC_STYLE_LINENUMBER, wxColour(35, 35, 35));
 
     // To make sure style is refreshed and also without this the background color of text is not as same as editor
     StyleClearAll();
     Refresh();
     Update();
+
+    // wxStyledTextCtrl *editor = new wxStyledTextCtrl(this, wxID_ANY);
+    // editor->StyleSetForeground(wxSTC_STYLE_DEFAULT, wxColour(200, 200, 200));
+    // editor->StyleSetBackground(wxSTC_STYLE_DEFAULT, wxColour(30, 30, 30));
+    // editor->StyleClearAll();
+    // editor->SetCaretForeground(wxColour(255, 255, 255));
+    // editor->SetMarginWidth(0, 50);
+    // editor->StyleSetForeground(wxSTC_STYLE_LINENUMBER, wxColour(75, 75, 75));
+    // editor->StyleSetBackground(wxSTC_STYLE_LINENUMBER, wxColour(35, 35, 35));
+
+    // Refresh();
+    // Update();
 }
 
 void Editor::Undo()
